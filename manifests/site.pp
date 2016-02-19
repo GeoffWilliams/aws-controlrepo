ded5c2c1851c3e2b053b2016463843e311481c4e
Geoff Williams <geoff.williams@puppetlabs.com>
2016-02-19 11:41:04 +1100

    classify via site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,1 +1,12 @@
 # site.pp
+
+# classify nodes based on role fact if present
+node default {
+  include role::fact_classified
+}
+
+
+# puppet master
+node "pe-puppet.localdomain" {
+  include role::puppet::master
+}
